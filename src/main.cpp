7f48ec2e3d539b905dd265db34eda8ed440e5171
Ivan Vorobyev <ivorob@gmail.com>
2022-09-23 21:56:13 +0300

    Add usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <filesystem>
 
-int main() {
-    std::cout << "uqtdeployer" << std::endl;
+namespace fs = std::filesystem;
+
+namespace {
+
+void usage() {
+    std::cout << "Usage:" << std::endl
+              << "\tuqtdeployer executable" << std::endl;
+}
+
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        usage();
+        return EXIT_FAILURE;
+    }
+
+    try {
+        std::string filename = argv[1];
+        if (!fs::exists(filename)) {
+            throw std::runtime_error("File '" + filename + "' isn't exists");
+        }
+
+        std::cout << "Handling file '" + filename + "'...\tOK" << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
     return EXIT_SUCCESS;
 }
